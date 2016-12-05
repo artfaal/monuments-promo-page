661785d2fb325fb7cbab86f7d2e4e09e17fd5afc
Artfaal <sys.dll@gmail.com>
2016-12-05 13:14:06 +0300

    Refactoring. Reorganize files

--- app.py
+++ app.py
@@ -4,9 +4,16 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
-def hello_world():
+def index():
     return render_template('index.html')
+
+
+@app.route('/<name_page>')
+def pages(name_page):
+    return render_template(name_page)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
